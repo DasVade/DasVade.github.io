19e40a2142cc1d71d5fd288f3cdd526f5454a084
DasVade <151929677+DasVade@users.noreply.github.com>
2025-10-29 05:12:12 +0000

    Deploying to gh-pages from @ DasVade/DasVade.github.io@70a6828f817329c12c78c4832f261048f7328609 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,14 +61,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-optimal-control-for-dynamic-systems",
+          title: 'Optimal Control for Dynamic Systems',
+          description: "Implementation of iLQR and DDP algorithms for nonlinear trajectory optimization.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-ur16e-manipulator-control",
@@ -76,21 +71,6 @@ ninja.data = [{
           description: "Model-based torque control and real-time trajectory optimization",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
